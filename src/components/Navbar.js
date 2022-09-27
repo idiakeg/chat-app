1220de40e4046fcfe295b04bcd8e284ba7ec8405
Godspower Idiakose <idiakeg@gmail.com>
2022-09-27 09:39:34 +0100

    Included a loading state when the user logouts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,40 +4,49 @@ import { doc, updateDoc } from "firebase/firestore";
 import { signOut } from "firebase/auth";
 import { useContext } from "react";
 import authContext from "../contexts/authContext";
+import { useState } from "react";
 
 const Navbar = () => {
+	const [isLoading, setIsLoading] = useState(false);
 	const { user } = useContext(authContext);
 
 	const history = useNavigate();
 
 	const handleLogOut = async () => {
+		setIsLoading(true);
 		await updateDoc(doc(db, "users", auth.currentUser.uid), {
 			isOnline: false,
 		});
 		await signOut(auth);
+		setIsLoading(false);
 		localStorage.setItem("isLoggedIn", false);
-		// redirecting user to login page
 		history("/login");
 	};
 
 	return (
 		<nav>
-			<h3>
-				<Link to="/">ChatApp</Link>{" "}
-			</h3>
-			<div>
-				{user ? (
-					<>
-						<Link to="/profile">Profile</Link>
-						<button onClick={handleLogOut}>Logout</button>
-					</>
-				) : (
-					<>
-						<Link to="/register">Register</Link>
-						<Link to="/login">Login</Link>
-					</>
-				)}
-			</div>
+			{isLoading ? (
+				<p>Loading . . .</p>
+			) : (
+				<>
+					<h3>
+						<Link to="/">ChatApp</Link>{" "}
+					</h3>
+					<div>
+						{user ? (
+							<>
+								<Link to="/profile">Profile</Link>
+								<button onClick={handleLogOut}>Logout</button>
+							</>
+						) : (
+							<>
+								<Link to="/register">Register</Link>
+								<Link to="/login">Login</Link>
+							</>
+						)}
+					</div>
+				</>
+			)}
 		</nav>
 	);
 };
