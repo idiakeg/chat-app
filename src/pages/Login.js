dd727f60f199b7ac2b3b991d87369883d95792a2
Godspower Idiakose <idiakeg@gmail.com>
2022-09-28 01:09:17 +0100

    Removed redundant import

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "../firebase";
