e0dd327f6ef5509014380be81c31422542145308
Godspower Idiakose <idiakeg@gmail.com>
2022-09-30 08:21:34 +0100

    first version to get the desired data from the users collection

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,29 @@
-import React from "react";
+import { useEffect, useContext } from "react";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+import authContext from "../contexts/authContext";
 
 const Home = () => {
+	const { user } = useContext(authContext);
+	useEffect(() => {
+		if (user) {
+			const q = query(
+				collection(db, "users"),
+				where("uid", "not-in", [user.uid])
+			);
+			// this runs just once and can be fine for other usage but i'd like to use a listener
+			getDocs(q)
+				.then((snapShot) => {
+					snapShot.docs.forEach((doc) => {
+						console.log(doc.data().uid);
+					});
+				})
+				.catch((err) => console.log(err.message));
+		} else {
+			console.log("No authenticated user");
+		}
+	}, [user]);
+
 	return <div>Home</div>;
 };
 
